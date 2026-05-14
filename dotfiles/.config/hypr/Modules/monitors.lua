4d794b6dc53aa34b1aec1ed2da19f2e7ab2c370c
Choc-Shake <dogking3601@gmail.com>
2026-05-13 23:13:56 -0600

    Syncing current, default configuration with repository.

---
+++ dotfiles/.config/hypr/Modules/monitors.lua
@@ -0,0 +1,7 @@
+--Monitor Configuration
+hl.monitor ({
+	output = "",             -- Replace "DP-1" with the name you found in Step 1
+   	mode = "highres@highrr",      -- Sets your native 1440p resolution at 200Hz
+   	position = "auto",            -- The coordinates of this screen
+  	scale = "auto", --scales monitor
+})
---
+++ dotfiles/.config/hypr/hyprland.lua
@@ -0,0 +1,12 @@
+
+
+
+require("Modules.autostart")
+require("Modules.enviroment_variables")
+require("Modules.input")
+require("Modules.keybinds")
+require("Modules.visuals")
+require("Modules.misc")
+require("Modules.monitors")
+require("Modules.permissions")
+require("Modules.windows_rules")
